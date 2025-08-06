2a7004c77ea07fd8d203b861fdd57f82259647b9
P Ghoradkar <praj12121998@gmail.com>
2025-08-06 19:33:26 -0400

    Go Microservice Project first commit - GraphQL

---
+++ graphql/account_resolver.go
@@ -0,0 +1,7 @@
+package main
+
+type accountResolver struct {
+	server *Server
+}
+
+
---
+++ graphql/mutation_resolver.go
@@ -0,0 +1,17 @@
+package main
+
+type mutationResolver struct {
+	server *Server
+}
+
+// func (r *mutationResolver) CreateAccount(ctx context.Context, input AccountInput) (*Account, error) {
+
+// }
+
+// func (r *mutationResolver) CreateProduct(ctx context.Context, input ProductInput) (*Product, error) {
+
+// }
+
+// func (r *mutationResolver) CreateOrder(ctx context.Context, input OrderInput) (*Order, error) {
+
+// }
